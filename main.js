02964629bc8e8baee2f51bb229f7d5328dde09b3
Sandra Eng <sandra.eng@live.no>
2022-12-17 12:48:23 +0100

    Third Commit

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 let seeds = 0;
 let money = 0;
+let clickCount = 1;
+
+let addClickPrice = 40;
 
 function waterPlants(){
-    seeds++
+    seeds += clickCount
     document.getElementById('wateredPlants').innerHTML = 'Seeds: ' + seeds
 }
 
@@ -13,4 +16,21 @@ function sellSeeds(){
         seeds = 0;
         document.getElementById('wateredPlants').innerHTML = 'Seeds: ' + seeds;
     }
+}
+
+function addClick(){
+    if(money >= addClickPrice){
+        clickCount += 1
+        money -= 30
+        document.getElementById('money').innerHTML = 'Money: ' + money + '$'
+        addClickPrice *= 2
+        document.getElementById('addClickPrice').innerHTML = 'Price: ' + addClickPrice + '$'
+    }
+}
+
+function hireWorkers(){
+    setInterval(function() {
+        seeds += 1;
+        document.getElementById('wateredPlants').innerHTML = 'Seeds: ' + seeds;
+      }, 1500);
 }
