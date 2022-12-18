5965842b981664feb54206e6b9a60ae1b7cc13e0
Sandra Eng <sandra.eng@live.no>
2022-12-18 09:59:21 +0100

    Cheats and fixes

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 let seeds = 0;
 let money = 0;
 let clickCount = 1;
-let clicksPerSeconds = 1;
+let clicksPerSeconds = 0;
 
 let addClickPrice = 40;
 let hireWorkersPrice = 150;
 
-function waterPlants(){
+function onLoad() {
+    document.addEventListener('keydown', cheatSeeds, false);
+}
+
+function waterPlants() {
     seeds += clickCount
     document.getElementById('wateredPlants').innerHTML = 'Seeds: ' + seeds
 }
 
-function sellSeeds(){
-    if(seeds >= 1) {
+function sellSeeds() {
+    if (seeds >= 1) {
         money += seeds
         document.getElementById('money').innerHTML = 'Money: ' + money + '$'
         seeds = 0;
@@ -20,8 +24,8 @@ function sellSeeds(){
     }
 }
 
-function addClick(){
-    if(money >= addClickPrice){
+function addClick() {
+    if (money >= addClickPrice) {
         clickCount += 1
         money -= 30
         document.getElementById('money').innerHTML = 'Money: ' + money + '$'
@@ -30,14 +34,33 @@ function addClick(){
     }
 }
 
-function hireWorkers(){
-    if(money >= hireWorkersPrice){
+function hireWorkers() {
+    if (money >= hireWorkersPrice) {
         money -= hireWorkersPrice
+        clicksPerSeconds += 1
+        document.getElementById('money').innerHTML = 'Money: ' + money + '$'
         setInterval(function() {
             seeds += clicksPerSeconds;
             document.getElementById('wateredPlants').innerHTML = 'Seeds: ' + seeds;
         }, 1500);
+        hireWorkersPrice *= 2
         document.getElementById('hireWorkersPrice').innerHTML = 'Price: ' + hireWorkersPrice + '$'
         document.getElementById('seedsPerSeconds').innerHTML = 'Seeds per second: ' + clicksPerSeconds;
     }
 }
+
+// keyboard handler function
+function cheatSeeds(event) {
+    if (event.key === 'a') {
+        seeds += 100;
+        document.getElementById('wateredPlants').innerHTML = 'Seeds: ' + seeds;
+    }
+    if (event.key ==='b'){
+        {
+            money += 500;
+            document.getElementById('money').innerHTML = 'Money: ' + money + '$';
+        }
+    }
+}
+
+window.onload = onLoad();
